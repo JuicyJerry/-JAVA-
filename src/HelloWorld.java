b0d0a8070974fa33e64865edd9796322c129b7c3
JuicyJerry <ju2cyjerry@gmail.com>
2020-03-12 18:32:33 +0900

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hell World");
+	}
+}
+// ctr + n (새 파일 만들기)
+// ctr + F11 (실행)

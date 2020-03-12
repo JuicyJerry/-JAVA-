375b1f24067fc35826a4d97dac86ad86fd45ff51
JuicyJerry <ju2cyjerry@gmail.com>
2020-03-12 18:41:09 +0900

    Hi World 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hell World");
+		System.out.println("Hi World");
 	}
 }
 // ctrl + n (새 파일 만들기)
 // ctrl + F11 (실행)
 // ctrl + s (저장)
+// commit 소스저장소에 저장하는 과정
